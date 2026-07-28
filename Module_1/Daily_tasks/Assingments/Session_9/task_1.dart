ac17464bc22088f7b9a98e9e5d7edaa8e73231c2
SnehHoriya-01 <snehhoriya82@gmail.com>
2026-07-28 11:07:49 +0530

    Create task_1.dart

---
+++ Module_1/Daily_tasks/Assingments/Session_9/task_1.dart
@@ -0,0 +1,10 @@
+void main()
+{
+    String foodname="Pizza";
+    double price=120.00;
+    bool vegetarian=true;
+
+    print("The food's name is: $foodname");
+    print("The food's price is: $price");
+    print("The food is veg: $vegetarian");
+}
