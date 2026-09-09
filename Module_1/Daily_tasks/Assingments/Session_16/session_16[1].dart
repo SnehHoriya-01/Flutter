bcfbfee14f7c163db7e670e208aff400ff3ce5de
SnehHoriya-01 <snehhoriya82@gmail.com>
2026-09-10 00:05:49 +0530

    Create session_16[1].dart
    This Dart program creates a map to store food item details and prints them.

---
+++ Module_1/Daily_tasks/Assingments/Session_16/session_16[1].dart
@@ -0,0 +1,12 @@
+void main()
+{
+  Map foodItem = Map();
+  foodItem["name"] = "pizza";
+  foodItem["price"] = "500";
+  foodItem["category"]  = "Fast Food";
+
+  print("Name: ${foodItem['name']}");
+  print("Price: ${foodItem['price']}");
+  print("Category: ${foodItem['category']}");
+
+}
